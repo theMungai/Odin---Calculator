fe7e552b0303bd79c49a2bde538d28ce533e2e96
theMungai <mungaithesimon@gmail.com>
2024-10-29 16:24:44 +0300

    Odin Calculator

--- script.js
+++ script.js
@@ -1,13 +1,34 @@
-document.addEventListener('DOMContentLoaded', () =>{
-    const previousCalculations = document.querySelector(".previous-calculations");
-    const currentAnswer = document.querySelector(".current-calculations");
-    const currentCalculations = document.querySelector(".current-answer");
-    const userInput = document.querySelector("#user-input");
-    const Numbers = document.querySelector(".numbers");
-    const mathOperrators = document.querySelector(".calculating-signs");
+const previousCalculations = document.querySelector(".previous-calculations");
+const currentCalculations = document.querySelector(".current-calculations");
+const currentAnswer = document.querySelector(".current-answer");
+const userInput = document.querySelector("#user-input");
+const clearAll = document.querySelector("#clear");
+const equalSign = document.querySelector("#calculate");
+const multiplyOperator = document.querySelector("#multiply-operator");
+const divideOperator = document.querySelector("#divide-operator");
+const addOperator = document.querySelector("#add-operator");
+const substractOperator = document.querySelector("#substract-operator");
+const deleteLeft = document.querySelector("#delete");
 
-    Numbers.addEventListener('click', () => {
-        userInput.textContent = Numbers
-    });
+function appendToDisplay(input){
+    userInput.value += input;
+}
+
+// clearing the display
+clearAll.addEventListener('click', () => {
+    userInput.value = "";
 
 });
+
+deleteLeft.addEventListener('click', () => {
+    
+})
+
+// Calculating any calculations from the user
+
+equalSign.addEventListener('click', () => {
+    if(divideOperator){
+        // currentAnswer.textContent = `${(userInput.value )/ (userInput.value)}`
+        userInput.textContent = (userInput.value )/ (userInput.value)
+    }
+})
