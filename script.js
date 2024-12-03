977330a03ef2a0e198eeb46650940a3dcea29d86
theMungai <mungaithesimon@gmail.com>
2024-12-03 10:48:10 +0300

    Data Attribute to the keys

--- script.js
+++ script.js
@@ -4,9 +4,24 @@ const displayScreen = document.querySelector(".display-screen");
 
 keys.forEach((key) => {
     key.addEventListener("click", (event) => {
-        const keyValue = event.target;
-        const displayValue = keyValue.textContent
-        console.log(displayValue)
-        displayScreen.value = displayValue
+        const clickedKey = event.target
+        const keyValue = clickedKey.textContent;
+        const display = displayScreen.textContent;
+        const displayValue = displayScreen.value;
+        
+        // Checking if the key value is a number
+        if(key.dataset.type === "numbers"){
+            if(displayValue === "0"){
+                displayScreen.value = keyValue
+            }
+            else {
+                displayScreen.value = displayValue + keyValue
+            }
+        }
+
+        // Checking if the key value is an operator
+        if(key.dataset.type === "operator"){
+            console.log(key)
+        }
     })
-})
+});
